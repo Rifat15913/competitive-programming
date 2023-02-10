aaedb48cab0fe70d41a07dd5ee2b083ae32913da
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2023-02-11 00:05:18 +0600

    create initial environment for competitive programming

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    string name;
+
+    cin >> name;
+    cout << name << endl;
+
+    return 0;
+}
